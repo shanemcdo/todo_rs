59af6b4ab3f2c5ef0f0e51919d37ab8d1082130f
Shane McDonough <shanemcdman@gmail.com>
2022-01-17 16:03:45 -0800

    Use termion

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,32 @@ use std::io::{
     BufRead,
     prelude::*,
 };
+use termion::{
+    self,
+    input::TermRead,
+    raw::IntoRawMode,
+};
+
+const TODO_LIST: &str = std::env!("TODO_LIST");
+const DONE_LIST: &str = std::env!("TODO_DONE_LIST");
+const COLORS_LEN: usize = 12;
+const COLORS: [Color; COLORS_LEN] = [
+    Color::TrueColor{r: 255, g: 0, b: 0},
+    Color::TrueColor{r: 255, g: 128, b: 0},
+    Color::TrueColor{r: 255, g: 255, b: 0},
+    Color::TrueColor{r: 128, g: 255, b: 0},
+    Color::TrueColor{r: 0, g: 255, b: 0},
+    Color::TrueColor{r: 0, g: 255, b: 128},
+    Color::TrueColor{r: 0, g: 255, b: 255},
+    Color::TrueColor{r: 0, g: 128, b: 255},
+    Color::TrueColor{r: 0, g: 0, b: 255},
+    Color::TrueColor{r: 128, g: 0, b: 255},
+    Color::TrueColor{r: 255, g: 0, b: 255},
+    Color::TrueColor{r: 255, g: 0, b: 128}
+];
 
 fn save_list(filename: &str, list: Vec<String>) {
-    let mut file = std::fs::File::open(filename)
-        .unwrap_or_else(|_| std::fs::File::create(filename).expect("Could not create file"));
+    let mut file = std::fs::File::create(filename).expect("Could not create file");
     for line in list {
         write!(file, "{}\n", line).expect("Could not write to file");
     }
@@ -25,9 +47,70 @@ fn load_list(filename: &str) -> Vec<String> {
     }
 }
 
+fn colorize(index: usize) -> Color {
+    COLORS[index % COLORS_LEN].clone()
+}
+
+fn colorize_list(list: Vec<String>) -> Vec<ColoredString> {
+    list.into_iter()
+        .enumerate()
+        .map(|(index, string)| string.color(colorize(index)))
+        .collect()
+}
+
+/// A program that acts as a todo list
+#[derive(Debug, StructOpt)]
+#[structopt(name = "todo")]
+struct Args {
+    /// Directly add an item to the todo list
+    #[structopt(short, long, default_value = "")]
+    add: String
+}
+
+struct ListApp {
+    running: bool,
+    stdin: termion::input::Keys<termion::AsyncReader>,
+    stdout: termion::raw::RawTerminal<io::Stdout>
+}
+
+impl ListApp{
+    fn new() -> Self{
+        Self {
+            running: true,
+            stdin: termion::async_stdin().keys(),
+            stdout: io::stdout().into_raw_mode().unwrap(),
+        }
+    }
+
+    fn run(&mut self) {
+        while self.running {
+            self.kbin();
+        }
+    }
+
+    fn kbin(&mut self) {
+        if let Some(Ok(key)) = self.stdin.next() {
+            match key {
+                termion::event::Key::Char(ch) => match ch {
+                    'q' => self.running = false,
+                    'h' => (),
+                    'j' => (),
+                    'k' => (),
+                    'l' => (),
+                    _ => (),
+                }
+                _ => (),
+            }
+        }
+    }
+}
+
 fn main() {
-    const TODO_LIST: &str = std::env!("TODO_LIST");
-    const DONE_LIST: &str = std::env!("TODO_DONE_LIST");
-    println!("{:?}", load_list(TODO_LIST));
-    println!("{:?}", load_list(DONE_LIST));
+    let args = Args::from_args();
+    if args.add != "" {
+        let mut list = load_list(TODO_LIST);
+        list.push(args.add);
+        save_list(TODO_LIST, list);
+    }
+    ListApp::new().run();
 }
